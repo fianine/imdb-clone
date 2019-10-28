95d04a1272eed6d8ac2398a18f8840dd75c02668
fianine <princealfiyana@gmail.com>
2019-10-28 11:52:02 +0000

    make auth

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,9 +3,7 @@
   <main role="main">
 		<section class="jumbotron text-center">
 			<div class="container">
-				<h1 class="jumbotron-heading">Album example</h1>
-				<p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-				<p><a class="btn btn-primary my-2" href="#">Main call to action</a> <a class="btn btn-secondary my-2" href="#">Secondary action</a></p>
+				<h1 class="jumbotron-heading">Movie List</h1>
 			</div>
 		</section>
 		<div class="album py-5 bg-light">
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,15 @@
 const mix = require('laravel-mix');
 
-// frontend
-mix.styles([
-   'public/frontend/css/bootstrap.min.css',
-   'public/frontend/css/style.css',
-],'public/css/main.css').version();
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
 
-mix.scripts([
-    'public/frontend/js/bootstrap.min.js',
-],'public/js/main.js').version();
+mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css');
